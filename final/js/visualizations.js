696871cff0f030ea1f6328b15a74d6af1c1d1527
Patrick Wied <patrick.wied@live.at>
2013-02-27 01:09:34 +0100

    init maps in final

--- final/js/visualizations.js
+++ final/js/visualizations.js
@@ -13,9 +13,18 @@
 
 	viz["CultureHotspots"] = (function(){
 
-		var init = function(){
-			
-		};
+		var	map = null,
+			init = function(){
+				map = L.map('cultureMap', {
+				    center: [51.505, -0.09],
+				    zoom: 13
+				});
+				L.tileLayer('http://maps1.wien.gv.at/wmts/lb/{Style}/{TileMatrixSet}/{TileMatrix}/{TileRow}/{TileCol}.jpeg', {
+				    style: 'farbe',
+				    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+				    maxZoom: 18
+				}).addTo(map);
+			};
 
 		return {
 			init: init
